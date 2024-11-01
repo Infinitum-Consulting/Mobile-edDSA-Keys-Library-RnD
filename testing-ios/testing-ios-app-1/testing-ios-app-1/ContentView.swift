d0feecc3b175447e454ac05e9596f0eb69f01b31
Yash Goyal <yashgo0018@gmail.com>
2024-11-02 02:35:00 +0530

    added code to generate, store and retreive private key from keychain

--- testing-ios/testing-ios-app-1/testing-ios-app-1/ContentView.swift
+++ testing-ios/testing-ios-app-1/testing-ios-app-1/ContentView.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import CryptoKit
+import Security
+import LocalAuthentication
 
 struct ContentView: View {
     var body: some View {
@@ -14,8 +17,112 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button("Show", action: click)
+                .padding(.horizontal)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .padding()
+            Button("Retreive", action: retrieve)
+                .padding(.horizontal)
+                .background(Color.red)
+                .foregroundColor(.white)
         }
         .padding()
+    }
+    
+    func retrieve() {
+        let tag = "consulting.infinitum.testing-ios-app-1.privatekey".data(using: .utf8)!
+        
+        let context = LAContext()
+        context.localizedReason = "Access your password on the keychain"
+                
+        let getQuery: [String: Any] = [
+            kSecClass as String:            kSecClassKey,
+            kSecAttrKeyType as String:      kSecAttrKeyTypeEC,
+            kSecAttrKeyClass as String:     kSecAttrKeyClassPrivate,
+            kSecAttrApplicationTag as String: tag,
+            kSecReturnData as String:       true,
+            kSecUseAuthenticationContext as String: context,
+        ]
+        do {
+            var item: CFTypeRef?
+            let status = SecItemCopyMatching(getQuery as CFDictionary, &item)
+            
+            if status == errSecSuccess {
+                if let privateKeyData = item as? Data {
+                    // Reconstruct the private key from the data
+                    let retrievedPrivateKey = try Curve25519.Signing.PrivateKey(rawRepresentation: privateKeyData)
+                    print(retrievedPrivateKey.publicKey)
+                    print("Private key retrieved successfully.")
+                    
+                    let message = "Hello, World!".data(using: .utf8)!
+                    let signature = try retrievedPrivateKey.signature(for: message)
+                    print(signature.base64EncodedString())
+                    // Use `retrievedPrivateKey` as needed
+                }
+            } else if status == errSecItemNotFound {
+                print("Private key not found in the Keychain.")
+            } else {
+                print("Error retrieving private key: \(status)")
+            }
+        } catch {
+            print("Error generating private key: \(error.localizedDescription)")
+        }
+    }
+    
+    func click() {
+        let key = Curve25519.Signing.PrivateKey()
+        let privateKeyData = key.rawRepresentation
+        let tag = "consulting.infinitum.testing-ios-app-1.privatekey".data(using: .utf8)!
+        
+        // Delete any existing key
+        // let deleteQuery: [String: Any] = [
+           //  kSecClass as String:            kSecClassKey,
+           //  kSecAttrKeyType as String:      kSecAttrKeyTypeEC,
+           //  kSecAttrKeyClass as String:     kSecAttrKeyClassPrivate,
+        // kSecAttrApplicationTag as String: tag
+        // ]
+        // SecItemDelete(deleteQuery as CFDictionary)
+        
+        var error: Unmanaged<CFError>?
+        let accessControl = SecAccessControlCreateWithFlags(
+            nil,
+            kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+            .biometryCurrentSet,
+            &error
+        )
+
+        if let error = error {
+            print("Error creating access control: \(error.takeRetainedValue() as Error)")
+            // Handle the error appropriately
+        }
+        
+        let context = LAContext()
+        context.touchIDAuthenticationAllowableReuseDuration = 10
+        
+        let addQuery: [String: Any] = [
+            kSecClass as String:            kSecClassKey,
+            kSecAttrKeyType as String:      kSecAttrKeyTypeEC,
+            kSecAttrKeyClass as String:     kSecAttrKeyClassPrivate,
+            kSecAttrApplicationTag as String: tag,
+            kSecValueData as String:        privateKeyData,
+            kSecUseAuthenticationContext as String: context as Any,
+            kSecAttrAccessControl as String:   accessControl as Any
+        ]
+        
+        let status = SecItemAdd(addQuery as CFDictionary, nil)
+
+        if status == errSecSuccess {
+            print("Private key stored successfully.")
+        } else if status == errSecDuplicateItem {
+            print("Private key already exists in the Keychain.")
+        } else {
+            print("Error storing private key: \(status)")
+        }
+        
+        
+        print(key.publicKey)
+        print(key.rawRepresentation)
     }
 }
 
