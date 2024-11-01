eb4fc8404d211977d04d0147ad5b9a7d12a62419
Yash Goyal <yashgo0018@gmail.com>
2024-11-02 01:13:39 +0530

    initial commit

---
+++ testing-ios/testing-ios-app-1/testing-ios-app-1Tests/testing_ios_app_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  testing_ios_app_1Tests.swift
+//  testing-ios-app-1Tests
+//
+//  Created by Yash Goyal on 02/11/24.
+//
+
+import Testing
+@testable import testing_ios_app_1
+
+struct testing_ios_app_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
