f1a17f514fdbd823eadfbd38a61eedb6036e71a3
Yash Goyal <yashgo0018@gmail.com>
2025-01-21 03:42:40 +0530

    added a new ios app for testing baby jub jub on ios

---
+++ testing-ios/testing-babyjubjub-implementations/testing-babyjubjub-implementations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testing-babyjubjub-implementations
+//
+//  Created by Yash Goyal on 21/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testing-ios/testing-babyjubjub-implementations/testing-babyjubjub-implementations/testing_babyjubjub_implementationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_babyjubjub_implementationsApp.swift
+//  testing-babyjubjub-implementations
+//
+//  Created by Yash Goyal on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct testing_babyjubjub_implementationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
